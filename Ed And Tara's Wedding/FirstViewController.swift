b05f426300e1ce20b512cffae7a8abe3bb5a850e
Edmund Dipple <edmundd@Edmunds-MacBook-Pro.local>
2015-07-25 15:11:53 +0100

    Initial Commit

---
+++ Ed And Tara's Wedding/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Ed And Tara's Wedding
+//
+//  Created by Edmund Dipple on 25/07/2015.
+//  Copyright (c) 2015 Edmund Dipple. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
