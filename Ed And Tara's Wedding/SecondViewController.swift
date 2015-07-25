7165353249221e7332317d83be871c4c79e3f7f2
Edmund Dipple <edmundd@Edmunds-MacBook-Pro.local>
2015-07-25 16:07:08 +0100

    Map view now centers in on Hatfield house

--- Ed And Tara's Wedding/SecondViewController.swift
+++ Ed And Tara's Wedding/SecondViewController.swift
@@ -7,12 +7,26 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // create a region and pass it to the Map View
+        var region = MKCoordinateRegion();
+        region.center.latitude = 51.760453;
+        region.center.longitude = -0.209228;
+        region.span.latitudeDelta = 0.011;
+        region.span.longitudeDelta = 0.011;
+        
+        mapView.setRegion(region, animated: true);
+        
     }
 
     override func didReceiveMemoryWarning() {
