9633c242a412d49fb6227392ed6f453477ea318f
Edmund Dipple <elmundio1987@gmail.com>
2015-11-08 13:33:43 +0000

    Alternate code for zooming into an image

--- Ed And Tara's Wedding/MapViewController.swift
+++ Ed And Tara's Wedding/MapViewController.swift
@@ -9,71 +9,90 @@
 import UIKit
 import MapKit
 
-let image = "map.jpg"
-var imageView: UIImageView!
-var scrollView: UIScrollView!
+let imageScrollLargeImageName = "map.jpg"
+
 
 class MapViewController: UIViewController, UIScrollViewDelegate {
     
-    @IBOutlet weak var containerView: UIView!
- 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        redraw()
-
-    }
-    
-    override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
-        super.didRotateFromInterfaceOrientation(fromInterfaceOrientation)
-        redraw()
-    }
+    @IBOutlet weak var imageConstraintRight: NSLayoutConstraint!
+    @IBOutlet weak var imageConstraintBottom: NSLayoutConstraint!
+    @IBOutlet weak var imageConstraintLeft: NSLayoutConstraint!
+    @IBOutlet weak var imageConstraintTop: NSLayoutConstraint!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var scrollView: UIScrollView!
+   
+    var lastZoomScale: CGFloat = -1
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        redraw()
-    }
-    
-    func redraw(){
-        for subview in containerView.subviews {
-            subview.removeFromSuperview()
-        }
         
-        imageView = UIImageView(image: UIImage(named: image))
-        
-        // 2
-        scrollView = UIScrollView(frame: view.bounds)
-        scrollView.backgroundColor = UIColor.blackColor()
-        // 3
-        
-        if(view.bounds.width <= 490 ){
-            scrollView.contentSize = imageView.bounds.size
-        }else{
-            scrollView.contentSize = view.bounds.size
-            imageView.contentMode = .ScaleAspectFit
-        }
-        
-        // 4
-        scrollView.addSubview(imageView)
-        containerView.addSubview(scrollView)
-        
-        imageView.frame = CGRectMake(0, 0, scrollView.contentSize.width, scrollView.contentSize.height)
-   
-        scrollView.maximumZoomScale = 1.0
-        scrollView.minimumZoomScale = 0.5
+        imageView.image = UIImage(named: imageScrollLargeImageName)
         scrollView.delegate = self
-        
-        
-        
-        let scrollPoint = CGPointMake(50, 0.0)
-        scrollView.setContentOffset(scrollPoint, animated: false)
-        scrollView.zoomScale = 1.0
-        
+        updateZoom()
     }
- 
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // Update zoom scale and constraints
+    // It will also animate because willAnimateRotationToInterfaceOrientation
+    // is called from within an animation block
+    //
+    // DEPRECATION NOTICE: This method is said to be deprecated in iOS 8.0. But it still works.
+    override func willAnimateRotationToInterfaceOrientation(
+        toInterfaceOrientation: UIInterfaceOrientation, duration: NSTimeInterval) {
+            
+            super.willAnimateRotationToInterfaceOrientation(toInterfaceOrientation, duration: duration)
+            updateZoom()
+    }
+    
+    func updateConstraints() {
+        if let image = imageView.image {
+            let imageWidth = image.size.width
+            let imageHeight = image.size.height
+            
+            let viewWidth = view.bounds.size.width
+            let viewHeight = view.bounds.size.height
+            
+            // center image if it is smaller than screen
+            var hPadding = (viewWidth - scrollView.zoomScale * imageWidth) / 2
+            if hPadding < 0 { hPadding = 0 }
+            
+            var vPadding = (viewHeight - scrollView.zoomScale * imageHeight) / 2
+            if vPadding < 0 { vPadding = 0 }
+            
+            imageConstraintLeft.constant = hPadding
+            imageConstraintRight.constant = hPadding
+            
+            imageConstraintTop.constant = vPadding
+            imageConstraintBottom.constant = vPadding
+            
+            // Makes zoom out animation smooth and starting from the right point not from (0, 0)
+            view.layoutIfNeeded()
+        }
+    }
+    
+    // Zoom to show as much image as possible unless image is smaller than screen
+    private func updateZoom() {
+        if let image = imageView.image {
+            var minZoom = min(view.bounds.size.width / image.size.width,
+                view.bounds.size.height / image.size.height)
+            
+            if minZoom > 1 { minZoom = 1 }
+            
+            scrollView.minimumZoomScale = minZoom
+            
+            // Force scrollViewDidZoom fire if zoom did not change
+            if minZoom == lastZoomScale { minZoom += 0.000001 }
+            
+            scrollView.zoomScale = minZoom
+            lastZoomScale = minZoom
+        }
+    }
+    
+    
+    // UIScrollViewDelegate
+    // -----------------------
+    
+    func scrollViewDidZoom(scrollView: UIScrollView) {
+        updateConstraints()
     }
     
     func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
