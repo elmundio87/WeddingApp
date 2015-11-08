6918d7878a40dacf6228174d5676102dcc2625ee
Edmund Dipple <elmundio1987@gmail.com>
2015-11-08 08:12:42 +0000

    Map image realigns itself when rotated. Added a map button in a top navigation bar

--- Ed And Tara's Wedding/MapViewController.swift
+++ Ed And Tara's Wedding/MapViewController.swift
@@ -14,18 +14,32 @@ var scrollView: UIScrollView!
 
 class MapViewController: UIViewController, UIScrollViewDelegate {
     
+    @IBOutlet weak var containerView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // 1
-        imageView = UIImageView(image: UIImage(named: image))
+        redraw()
 
+    }
+    
+    override func didRotateFromInterfaceOrientation(fromInterfaceOrientation: UIInterfaceOrientation) {
+        super.didRotateFromInterfaceOrientation(fromInterfaceOrientation)
+        redraw()
+    }
+    
+    func redraw(){
+        for subview in containerView.subviews {
+            subview.removeFromSuperview()
+        }
+        
+        imageView = UIImageView(image: UIImage(named: image))
+        
         // 2
         scrollView = UIScrollView(frame: view.bounds)
         scrollView.backgroundColor = UIColor.whiteColor()
         // 3
-        print(imageView.bounds.width)
         
-        if(view.bounds.width <= 490){
+        if(view.bounds.width <= 490 ){
             scrollView.contentSize = imageView.bounds.size
         }else{
             scrollView.contentSize = view.bounds.size
@@ -34,14 +48,14 @@ class MapViewController: UIViewController, UIScrollViewDelegate {
         
         // 4
         scrollView.addSubview(imageView)
-        view.addSubview(scrollView)
+        containerView.addSubview(scrollView)
         
         imageView.frame = CGRectMake(0, 0, scrollView.contentSize.width, scrollView.contentSize.height)
         scrollView.maximumZoomScale = 5.0
         scrollView.minimumZoomScale = 0.5
         scrollView.delegate = self
     }
-    
+ 
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
