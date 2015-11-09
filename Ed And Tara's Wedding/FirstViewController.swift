10a56334dc69b2b404c9d3482403b6b8c3570884
Edmund Dipple <elmundio1987@gmail.com>
2015-11-09 08:36:37 +0000

    Added a 20 second timeout to the RSVP web form, and a way of refreshing the page.

--- Ed And Tara's Wedding/FirstViewController.swift
+++ Ed And Tara's Wedding/FirstViewController.swift
@@ -11,8 +11,12 @@ import QuartzCore
 
 class FirstViewController: UIViewController, UIWebViewDelegate {
 
+    @IBOutlet weak var connectionTimeoutLabel: UILabel!
+    @IBOutlet weak var refreshButton: UIButton!
     @IBOutlet weak var loadingView: UIView!
     @IBOutlet weak var webView: UIWebView!
+    var timer = NSTimer()
+    let connectionTimeout = 20.0
     
     var urlPath = "https://goo.gl/forms/M6g53EQEai"
     
@@ -34,12 +38,30 @@ class FirstViewController: UIViewController, UIWebViewDelegate {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func refreshButtonTouchUpInside(sender: AnyObject) {
+        webView.stopLoading()
+        loadAddressUrl()
+        refreshButton.hidden = true
+        connectionTimeoutLabel.hidden = true
+    }
 
     
     func webViewDidFinishLoad(webView: UIWebView ) {
         loadingView.hidden = true
+        timer.invalidate()
     }
 
+    func webViewDidStartLoad(webView: UIWebView) {
+        loadingView.hidden = false
+        timer = NSTimer.scheduledTimerWithTimeInterval(connectionTimeout, target: self, selector: "cancelWeb:", userInfo: nil, repeats: false)
+    }
+    
+    func cancelWeb(timer: NSTimer){
+        loadingView.hidden = true
+        refreshButton.hidden = false
+        connectionTimeoutLabel.hidden = false
+    }
     
 
 }
