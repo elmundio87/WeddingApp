2656fdc62631773bdb46a6bc9429ed05b9c0a366
Edmund Dipple <elmundio1987@gmail.com>
2015-11-10 12:35:42 +0000

    Added some initial code for a QR reader http://www.appcoda.com/qr-code-reader-swift/

--- Ed And Tara's Wedding/PasswordViewController.swift
+++ Ed And Tara's Wedding/PasswordViewController.swift
@@ -7,15 +7,65 @@
 //
 
 import UIKit
+import AVFoundation
 
 
-
-class PasswordViewController: UIViewController {
+class PasswordViewController: UIViewController,AVCaptureMetadataOutputObjectsDelegate {
+    
+    @IBOutlet weak var scanButton: UIButton!
+    var captureSession:AVCaptureSession?
+    var videoPreviewLayer:AVCaptureVideoPreviewLayer?
+    var qrCodeFrameView:UIView?
     
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("MainTabViewController") as UIViewController
-        self.presentViewController(viewController, animated: false, completion: nil)
+        //let viewController:UIViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewControllerWithIdentifier("MainTabViewController") as UIViewController
+        //self.presentViewController(viewController, animated: false, completion: nil)
     }
     
+    @IBAction func pressScanButton(sender: AnyObject) {
+        
+        // Get an instance of the AVCaptureDevice class to initialize a device object and provide the video
+        // as the media type parameter.
+        let captureDevice = AVCaptureDevice.defaultDeviceWithMediaType(AVMediaTypeVideo)
+        
+        // Get an instance of the AVCaptureDeviceInput class using the previous device object.
+       
+        var input: AnyObject!
+        do{
+            try input = AVCaptureDeviceInput(device: captureDevice)
+        }catch let error as NSError {
+            // If any error occurs, simply log the description of it and don't continue any more.
+            print("\(error.localizedDescription)")
+            return
+        }
+        
+        // Initialize the captureSession object.
+        captureSession = AVCaptureSession()
+        // Set the input device on the capture session.
+        captureSession?.addInput(input as! AVCaptureInput)
+        
+        // Initialize a AVCaptureMetadataOutput object and set it as the output device to the capture session.
+        let captureMetadataOutput = AVCaptureMetadataOutput()
+        captureSession?.addOutput(captureMetadataOutput)
+        
+        // Set delegate and use the default dispatch queue to execute the call back
+        captureMetadataOutput.setMetadataObjectsDelegate(self, queue: dispatch_get_main_queue())
+        captureMetadataOutput.metadataObjectTypes = [AVMetadataObjectTypeQRCode]
+        
+        // Initialize the video preview layer and add it as a sublayer to the viewPreview view's layer.
+        videoPreviewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
+        videoPreviewLayer?.videoGravity = AVLayerVideoGravityResizeAspectFill
+        videoPreviewLayer?.frame = view.layer.bounds
+        view.layer.addSublayer(videoPreviewLayer!)
+        
+        captureSession?.startRunning()
+        
+        // Initialize QR Code Frame to highlight the QR code
+        qrCodeFrameView = UIView()
+        qrCodeFrameView?.layer.borderColor = UIColor.greenColor().CGColor
+        qrCodeFrameView?.layer.borderWidth = 2
+        view.addSubview(qrCodeFrameView!)
+        view.bringSubviewToFront(qrCodeFrameView!)
+    }
 }
