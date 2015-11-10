669c0aff77c227c2344c8f828dfed65a0e32fb2f
Edmund Dipple <elmundio1987@gmail.com>
2015-11-10 09:57:36 +0000

    Added a venue website tab

---
+++ Ed And Tara's Wedding/VenueViewController.swift
@@ -0,0 +1,80 @@
+//
+//  VenueViewController.swift
+//  Ed And Tara's Wedding
+//
+//  Created by Edmund Dipple on 09/11/2015.
+//  Copyright © 2015 Edmund Dipple. All rights reserved.
+//
+
+import UIKit
+
+
+class VenueViewController: UIViewController, UIWebViewDelegate {
+    
+
+    @IBOutlet weak var connectionTimeoutLabel: UILabel!
+    @IBOutlet weak var loadingView: UIView!
+    @IBOutlet weak var webView: UIWebView!
+    @IBOutlet weak var refreshButton: UIButton!
+    
+    var timer = NSTimer()
+    let connectionTimeout = 20.0
+    var loaded = false;
+    
+    var urlPath = "http://hatfield-house.co.uk"
+    
+    func loadAddressUrl() {
+        let requestUrl = NSURL(string: urlPath)
+        let request = NSURLRequest(URL: requestUrl!)
+        webView.loadRequest(request)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadingView.layer.cornerRadius = 10.0
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        webView.delegate = self
+        if(!loaded){
+            loadAddressUrl()
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func refreshButtonTouchUpInside(sender: AnyObject) {
+        webView.stopLoading()
+        loadAddressUrl()
+        refreshButton.hidden = true
+        connectionTimeoutLabel.hidden = true
+    }
+    
+    
+    func webViewDidFinishLoad(webView: UIWebView ) {
+        loaded = true
+        loadingView.hidden = true
+        timer.invalidate()
+    }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        loadingView.hidden = false
+        timer = NSTimer.scheduledTimerWithTimeInterval(connectionTimeout, target: self, selector: "cancelWeb:", userInfo: nil, repeats: false)
+    }
+    
+    func cancelWeb(timer: NSTimer){
+        loadingView.hidden = true
+        refreshButton.hidden = false
+        connectionTimeoutLabel.hidden = false
+        webView.loadRequest(NSURLRequest(URL: NSURL(string: "about:blank")!))
+    }
+  
+    
+}
