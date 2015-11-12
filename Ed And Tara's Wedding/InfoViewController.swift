7b18364ca31d16dd60041a81bcaa42f5b85dd20b
Edmund Dipple <elmundio1987@gmail.com>
2015-11-12 19:33:06 +0000

    Open external links in Safari

--- Ed And Tara's Wedding/InfoViewController.swift
+++ Ed And Tara's Wedding/InfoViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class InfoViewController: UIViewController {
+class InfoViewController: UIViewController, UIWebViewDelegate {
     
     @IBOutlet weak var infoWebView: UIWebView!
     
@@ -16,6 +16,7 @@ class InfoViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         loadInfoFile()
+        infoWebView.delegate = self
     }
     
     func loadInfoFile() {
@@ -29,6 +30,16 @@ class InfoViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func webView(webView: UIWebView, shouldStartLoadWithRequest request: NSURLRequest, navigationType: UIWebViewNavigationType) -> Bool {
+        
+        let url: NSURL = request.URL!
+        let isExternalLink: Bool = url.scheme == "http" || url.scheme == "https" || url.scheme == "mailto"
+        if (isExternalLink && navigationType == UIWebViewNavigationType.LinkClicked) {
+            return !UIApplication.sharedApplication().openURL(request.URL!)
+        } else {
+            return true
+        }
+    }
     
     
     
